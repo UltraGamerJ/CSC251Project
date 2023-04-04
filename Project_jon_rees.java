7f3fa6d9c9b1060da047ef29e4b2ebd39e24d00d
Jon Rees <jonsamuelrees@gmail.com>
2023-04-04 14:06:20 -0400

    Update Project_jon_rees.java
    Modified to read input from a file

--- Project_jon_rees.java
+++ Project_jon_rees.java
@@ -1,87 +1,63 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Project_jon_rees {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         // * Initialization
 
-        Scanner scnr = new Scanner(System.in);
+        File policyFile = new File("PolicyInformation.txt");
+        Scanner scnr = new Scanner(policyFile);
+
+        List<Policy> policies = new ArrayList<Policy>();
 
         // * Input
 
-        // Get the policy number
-        System.out.print("Please enter the Policy Number: ");
-        int policyNumber = scnr.nextInt();
+        // While there are Policies to be read, load them into the list
+        while (scnr.hasNext()) {
 
-        // Get the policy provider name
-        System.out.print("Please enter the Provider Name: ");
-        scnr.nextLine(); // Consume extra linebreak char
-        String policyProvider = scnr.nextLine();
+            // Get the policy number
+            int policyNumber = scnr.nextInt();
 
-        // Get the policyholder's first name
-        System.out.print("Please enter the Policyholder's First Name: ");
-        String policyFirst = scnr.nextLine();
+            scnr.nextLine(); // Consume extra linebreak char
 
-        // Get the policyholder's last name
-        System.out.print("Please enter the Policyholder's Last Name: ");
-        String policyLast = scnr.nextLine();
+            // Get the policy provider name
+            String policyProvider = scnr.nextLine();
 
-        // Get the policyholder's age
-        System.out.print("Please enter the Policyholder's Age: ");
-        int policyAge = scnr.nextInt();
+            // Get the policyholder's first name
+            String policyFirst = scnr.nextLine();
 
-        // Get the policyholder's smoking status
-        System.out.print("Please enter the Policyholder's Smoking Status (smoker/non-smoker): ");
-        scnr.nextLine(); // Consume extra linebreak char
-        String policySmoker = scnr.nextLine();
+            // Get the policyholder's last name
+            String policyLast = scnr.nextLine();
 
-        // Get the policyholder's height
-        System.out.print("Please enter the Policyholder's Height (in inches): ");
-        double policyHeight = scnr.nextDouble();
+            // Get the policyholder's age
+            int policyAge = scnr.nextInt();
 
-        // Get the policyholder's weight
-        System.out.print("Please enter the Policyholder's Weight (in pounds): ");
-        double policyWeight = scnr.nextDouble();
+            scnr.nextLine(); // Consume extra linebreak char
 
-        // * Create Object
-        Policy myPolicy = new Policy(policyNumber, policyProvider, policyFirst, policyLast, policyAge, policySmoker, policyHeight, policyWeight);
+            // Get the policyholder's smoking status
+            String policySmoker = scnr.nextLine();
 
-        // * Output
+            // Get the policyholder's height
+            double policyHeight = scnr.nextDouble();
 
-        // ? Spacing
-        System.out.println();
+            // Get the policyholder's weight
+            double policyWeight = scnr.nextDouble();
 
-        // ? Data
-        // Policy number output
-        System.out.println("Policy Number: " + myPolicy.getNumber());
+            // * Create Object
+            Policy myPolicy = new Policy(policyNumber, policyProvider, policyFirst, policyLast, policyAge, policySmoker, policyHeight, policyWeight);
 
-        // Policy provider output
-        System.out.println("Provider Name: " + myPolicy.getProvider());
+            // Insert the object into the list
+            policies.add(myPolicy);
 
-        // Policyholder first name output
-        System.out.println("Policyholder's First Name: " + myPolicy.getFirstName());
+        }
 
-        // Policyholder last name output
-        System.out.println("Policyholder's Last Name: " + myPolicy.getLastName());
-
-        // Policyholder age output
-        System.out.println("Policyholder's Age: " + myPolicy.getAge());
-
-        // Policyholder smoking status output
-        System.out.println("Policyholder's Smoking Status: " + myPolicy.getSmoker());
-
-        // Policyholder height output
-        System.out.println("Policyholder's Height: " + String.format("%.1f", myPolicy.getHeight()) + " inches");
-
-        // Policyholder weight output
-        System.out.println("Policyholder's Weight: " + String.format("%.1f", myPolicy.getWeight()) + " pounds");
-
-        // Policyholder BMI output
-        System.out.println("Policyholder's BMI: " + String.format("%.2f", myPolicy.getBMI()));
-
-        // Policy price output
-        System.out.println("Policy Price: $" + String.format("%.2f", myPolicy.getPrice()));
+        // * Cleanup
+        scnr.close();
 
     }
 
